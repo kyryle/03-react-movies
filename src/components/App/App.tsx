9d537c865b86fccb0196a78394091ecdffb21386
user <kyryle0110@gmail.com>
2026-08-14 21:09:24 +0300

    check

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,10 @@
 import css from './App.module.css'
-import SearchBar from '../../SearchBar/SearchBar'
+import SearchBar from '../SearchBar/SearchBar'
 import { FetchMovies } from "../../services/movieService";
-import MovieList from "../../MovieGrid/MovieGrid";
-import Loader from "../../Loader/Loader";
-import ErrorMessage from "../../ErrorMessage/ErrorMessage";
-import MovieModal from "../MovieModal/MovieModal";
+import MovieList from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../../MovieModal/MovieModal";
 import { Toaster } from "react-hot-toast";
 
 const no = false
