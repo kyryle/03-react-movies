71a3e0586c2df4a4976cec129492be33041f8a4d
kyryle <60917199+kyryle@users.noreply.github.com>
2026-08-15 17:06:06 +0300

    "folder name change"

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import { FetchMovies } from "../../services/movieService";
 import MovieList from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
-import MovieModal from "../movieModal/MovieModal";
+import MovieModal from "../MovieModal/MovieModal";
 import { Toaster } from "react-hot-toast";
 
 const no = false
